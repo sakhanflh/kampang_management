3aa2d0fe217e917e798fd31b94ba7783136a799e
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2023-12-08 16:10:58 +0700

    menambahkan style pada css dan html januari

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const nav = document.querySelector("nav");
 window.addEventListener('scroll', function(){
     nav.classList.toggle('nav-active', window.scrollY > 0);
 })
+
+// Slider
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 4000);
+
