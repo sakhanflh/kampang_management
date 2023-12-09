02ba670115575d600447576f65b5d7a55e41ff0e
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2023-12-10 04:19:22 +0700

    {

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const nav = document.querySelector("nav");
+const navbar = document.querySelector(".navbar-mobile");
+const hamburgerButton = document.querySelector('.navbar-extra');
 
 window.addEventListener('scroll', function(){
     nav.classList.toggle('nav-active', window.scrollY > 0);
 })
+
+// Menu
+hamburgerButton.addEventListener('click', function(){
+    navbar.classList.toggle('navbar-reveal');
+});
+// Menu
 
 // Slider
 let counter = 1;
